e7a7f8fe26a60ad2eddbb44f76712df34b233f2e
mixxuss <mixxuss@gmail.com>
2017-01-23 17:42:32 +0300

    Added get_courses_info func

--- coursera.py
+++ coursera.py
@@ -14,8 +14,20 @@ def get_courses_list(url, courses_amount):
     return random.sample(courses_list, courses_amount)
 
 
-def get_course_info(course_slug):
-    pass
+def get_course_info(courses_url_list):
+    all_courses_info_list = []
+    for course_url in courses_url_list:
+        course_request = requests.get(course_url)
+        course_page = lxml.html.document_fromstring(course_request.text)
+        course_name = course_page.find_class('title display-3-text')[0].text_content()
+        course_lang = course_page.find_class('language-info')[0].text_content()
+        course_starts = course_page.find_class('startdate rc-StartDateString caption-text')[0].text_content()
+        course_length = len(course_page.find_class('week'))
+        course_rating = course_page.find_class('ratings-text bt3-visible-xs')[0].text_content()
+        course_sum = (course_url, course_name, course_lang, course_starts, course_length, course_rating)
+        all_courses_info_list.append(course_sum)
+    return all_courses_info_list
+
 
 
 def output_courses_info_to_xlsx(filepath):
@@ -24,5 +36,13 @@ def output_courses_info_to_xlsx(filepath):
 
 if __name__ == '__main__':
     url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    courses_amount = 20
-    # print(get_courses_list(url, courses_amount))
+    courses_amount = 3
+    print(get_course_info(get_courses_list(url, courses_amount)))
+    # course_url = 'https://www.coursera.org/learn/story-writing-project'
+    # course_html = requests.get(course_url)
+    # page_1 = lxml.html.document_fromstring(course_html.text)
+    # print(len(page_1.find_class('week')))
+    # print(page_1.find_class('ratings-text bt3-visible-xs')[0].text_content())
+    # print(page_1.find_class('startdate rc-StartDateString caption-text')[0].text_content())
+    # print(page_1.find_class('language-info')[0].text_content())
+    # print(page_1.find_class('title display-3-text')[0].text_content())
