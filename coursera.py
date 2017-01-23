81629b8af010bd56010bb8a6b5c24b2a9d0272a6
mixxuss <mixxuss@gmail.com>
2017-01-23 15:43:11 +0300

    Added get_courses_list func

--- coursera.py
+++ coursera.py
@@ -1,7 +1,17 @@
+from lxml import etree
+import lxml.html
+import random
+import requests
 
 
-def get_courses_list():
-    pass
+def get_courses_list(url, courses_amount):
+    xml_request = requests.get(url)
+    root = etree.XML(xml_request.content)
+    courses_list = []
+    for child in root:
+        for link in child:
+            courses_list.append(link.text)
+    return random.sample(courses_list, courses_amount)
 
 
 def get_course_info(course_slug):
@@ -13,4 +23,6 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    courses_amount = 20
+    # print(get_courses_list(url, courses_amount))
